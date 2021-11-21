bd0b4bb656f97bfd09fecd057adbee10f8ce15d8
Marciano M oreno <marciano_moreno@outlook.com>
2021-11-21 12:14:28 -0600

    for/else practice

---
+++ 1-Introduction/percussion_instruments.py
@@ -0,0 +1,8 @@
+percussion_instruments = ["Drum set", "Djembé", "Electric Bass", "Cymbal", "Marimba", "Shaker", "Vibraphone", "Claves"]
+for instrument in percussion_instruments:
+    #TODO: Implementa estatuto if/else para validar que no haya instrumentos de cuerdas. De momento solo: "Electric Bass".
+    #TODO: Si existe un instrumento no percusivo, repórtalo en la consolo y rompe el ciclo de procesamiento con `break`.
+    #TODO: Implementa el estatuto else para que el mensaje "So glad, just drums in this list!"
+    #       solo se muestre tras validar que todos los instrumentos de la colección son de percusión.
+    print("So glad, just drums in this list!")
+print("We are all done here!")
