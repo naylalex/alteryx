b43f356f065e1bd956b8886f1eedd3eaea73fc8c
Marciano M oreno <marciano_moreno@outlook.com>
2021-11-21 15:03:51 -0600

    Lab: function with single parameter

---
+++ 1-Introduction/fechas.py
@@ -0,0 +1,11 @@
+#TODO: Implementa una funcion que regrese el día de la semana en español
+def dia_semana(num_dia):
+    """
+    Parámetros: num_dia: entero
+    Regresa: string: día de la semana en español considerando la siguiente relación:
+    0: Lunes
+    1: Martes
+    ...
+    6: Domingo
+    """
+    pass
