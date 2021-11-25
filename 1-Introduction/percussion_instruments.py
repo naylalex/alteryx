718c32bc2d1b63f2dd5541e62825a482f85bd560
Marciano M oreno <marciano_moreno@outlook.com>
2021-11-24 19:14:36 -0600

    editorial review

--- 1-Introduction/percussion_instruments.py
+++ 1-Introduction/percussion_instruments.py
@@ -1,7 +1,7 @@
 percussion_instruments = ["Drum set", "Djembé", "Electric Bass", "Cymbal", "Marimba", "Shaker", "Vibraphone", "Claves"]
 for instrument in percussion_instruments:
     #TODO: Implementa estatuto if/else para validar que no haya instrumentos de cuerdas. De momento solo: "Electric Bass".
-    #TODO: Si existe un instrumento no percusivo, repórtalo en la consolo y rompe el ciclo de procesamiento con `break`.
+    #TODO: Si existe un instrumento no percusivo, imprímelo en la consolo y rompe el ciclo de procesamiento con `break`.
     #TODO: Si el instrumento es de percusión, despliega un mensaje en la consola indicando el nombre del instrumento.
     #TODO: Implementa el estatuto else para que el mensaje "So glad, just drums in this list!"
     #       solo se muestre tras validar que todos los instrumentos de la colección son de percusión.
